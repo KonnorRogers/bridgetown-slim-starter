8f5f8fe18700b858aac87ba6678dd4055ae76e3e
ParamagicDev <konnor5456@gmail.com>
2021-01-03 20:38:28 -0500

    initial commit

---
+++ snowpack.config.js
@@ -0,0 +1,47 @@
+const mount = {
+  frontend: '/_frontend_',
+  output: '/'
+}
+
+const proxy = {
+  // Account for users using Docker
+  '0.0.0.0:4000': '0.0.0.0:4000'
+}
+
+const plugins = [
+  [
+    '@snowpack/plugin-run-script',
+    {
+      cmd: 'bundle exec bridgetown build',
+      watch: '$1 --watch'
+    }
+  ],
+  ['@snowpack/plugin-babel'],
+  ['@snowpack/plugin-build-script', { cmd: 'postcss', input: ['.css'], output: ['.css'] }]
+]
+
+const installOptions = {
+  NODE_ENV: true
+}
+
+const devOptions = {
+  // Default port of bridgetown
+  port: 4000,
+  open: 'none',
+  // Default output directory of Bridgetown
+  out: 'output'
+}
+
+const buildOptions = {
+  clean: true,
+  metaDir: ''
+}
+
+module.exports = {
+  mount,
+  proxy,
+  plugins,
+  installOptions,
+  devOptions,
+  buildOptions
+}
