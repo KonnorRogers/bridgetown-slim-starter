3edcc350855110917edb7f7662f2bb1f658df6c1
ParamagicDev <konnor5456@gmail.com>
2021-01-03 21:02:17 -0500

    eslint + prettier

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,7 +3,7 @@ module.exports = {
     browser: true,
     es2021: true,
   },
-  extends: ["standard"],
+  extends: ["standard", "prettier"],
   parserOptions: {
     ecmaVersion: 12,
     sourceType: "module",
