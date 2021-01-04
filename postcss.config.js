d48c7012501cd9817fad0d3569fc50dad11fe189
ParamagicDev <konnor5456@gmail.com>
2021-01-04 18:30:58 -0500

    purging works!

--- postcss.config.js
+++ postcss.config.js
@@ -1,26 +1,26 @@
-// let purgecss = {};
-// if (process.env.NODE_ENV === "production") {
-//   purgecss = {
-//     content: [
-//       "./output/**/*.html",
-//       "./frontend/**/*.js",
-//       "./src/**/*.md",
-//       "./src/**/*.html",
-//       "./src/**/*.liquid",
-//     ],
-//     defaultExtractor: (content) => {
-//       const broadMatches = content.match(/[^<>"'`\s]*[^<>"'`\s:]/g) || [];
-//       const innerMatches = content.match(/[^<>"'`\s.()]*[^<>"'`\s.():]/g) || [];
-//       return broadMatches.concat(innerMatches);
-//     },
-//   };
-// }
+let purgecss = {};
+if (process.env.NODE_ENV === "production") {
+  purgecss = {
+    content: [
+      "./output/**/*.html",
+      "./frontend/**/*.js",
+      "./src/**/*.md",
+      "./src/**/*.html",
+      "./src/**/*.liquid",
+    ],
+    defaultExtractor: (content) => {
+      const broadMatches = content.match(/[^<>"'`\s]*[^<>"'`\s:]/g) || [];
+      const innerMatches = content.match(/[^<>"'`\s.()]*[^<>"'`\s.():]/g) || [];
+      return broadMatches.concat(innerMatches);
+    },
+  };
+}
 
 module.exports = {
-  plugins: {
-    "postcss-import": {},
-    tailwindcss: {},
-    autoprefixer: {},
-    // "@fullhuman/postcss-purgecss": {},
-  },
+  plugins: [
+    require("postcss-import"),
+    require("tailwindcss"),
+    require("autoprefixer"),
+    require("@fullhuman/postcss-purgecss")(purgecss)
+  ],
 };
