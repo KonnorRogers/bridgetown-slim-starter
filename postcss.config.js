885fd2645c840a3ba66c1ff6a96e83e0ee968c51
ParamagicDev <konnor5456@gmail.com>
2021-01-06 00:36:21 -0500

    purging now works

--- postcss.config.js
+++ postcss.config.js
@@ -1,7 +1,30 @@
+const plugins = {
+  "postcss-import": {},
+  "tailwindcss": {},
+  "autoprefixer": {},
+}
+
+if (process.env.NODE_ENV === "production") {
+  const purgecss = {
+    content: [
+      "./output/**/*.html",
+      "./frontend/**/*.js",
+      "./src/**/*.md",
+      "./src/**/*.html",
+      "./src/**/*.slim",
+    ],
+    defaultExtractor: (content) => {
+      const broadMatches = content.match(/[^<>"'`\s]*[^<>"'`\s:]/g) || [];
+      const innerMatches = content.match(/[^<>"'`\s.()]*[^<>"'`\s.():]/g) || [];
+      return broadMatches.concat(innerMatches);
+    },
+  };
+
+  plugins["@fullhuman/postcss-purgecss"] = purgecss;
+}
+
+
+
 module.exports = {
-  plugins: {
-    "postcss-import": {},
-    "tailwindcss": {},
-    "autoprefixer": {}
-  },
+  plugins
 };
