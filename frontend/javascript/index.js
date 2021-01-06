58779fff2aa1323bed25dd6174b97167c5b10a5f
ParamagicDev <konnor5456@gmail.com>
2021-01-05 23:35:40 -0500

    working on fixes

--- frontend/javascript/index.js
+++ frontend/javascript/index.js
@@ -1,2 +1,2 @@
-import'../styles/index.css'
+import "../styles/index.css"
 console.log("hi")
--- snowpack.config.js
+++ snowpack.config.js
@@ -1,12 +1,12 @@
 const mount = {
   frontend: "/frontend",
   output: "/",
-  public: { url: "/", static: true, resolve: false }
 };
 
 const plugins = [
-  ['@snowpack/plugin-build-script', { cmd: 'postcss', input: ['.css'], output: ['.css'] }],
-  ['@snowpack/plugin-run-script', { cmd: 'bridgetown build', watch: '$1 --watch' }]
+  // ['@snowpack/plugin-build-script', { cmd: 'postcss', input: ['.css'], output: ['.css'] }],
+  ["@snowpack/plugin-postcss"],
+  ['@snowpack/plugin-run-script', { cmd: 'bundle exec bridgetown build', watch: '$1 --watch' }]
 ];
 
 const installOptions = {
@@ -15,8 +15,7 @@ const installOptions = {
 
 const devOptions = {
   port: 4000,
-  hmrDelay: 400,
-  open: "none",
+  hmrDelay: 1000,
   output: "stream",
   baseUrl: "/",
 };
